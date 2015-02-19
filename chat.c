b05e33c95d42caf64689a1d7b531bfd67e1a6581
bhcarr <bhmcarr@gmail.com>
2015-02-19 16:30:27 -0500

    initial socket creation and binding

--- chat.c
+++ chat.c
@@ -14,12 +14,21 @@ int main(int argc, char* argv[]){
 	//check for -s or -c flag
 	int errno;
 	char* cmdbuf;
-	cmdbuf = (char *)malloc(BUF_SIZE);
+	cmdbuf = malloc(BUF_SIZE);
 	cmdbuf[BUF_SIZE] = '\0';
+
 
 	char * user_ip;
 	user_ip = malloc(20);
 	user_ip = "0.0.0.0";
+
+	typedef struct sockaddr_in {
+		short int sin_family;			// Address family, AF_INET
+		unsigned short int sin_port;	// Port number
+		struct in_addr sin_addr;		// Internet address
+		unsigned char sin_zero[8];		// sizeof(sockaddr) and all 0s
+	}sockaddr_in;
+
 
 //	flag1 = getopt(argc,argv,"SsCc");
 
@@ -36,11 +45,48 @@ int main(int argc, char* argv[]){
 
 	if(argv[2]){
 		sscanf(argv[2], "%i", &port);
-		printf("Using port number %d\n",port);
 	}
 
+	// create server socket -- call socket() and return a file descriptor
+	// of the new socket
+	int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_socket < 0){
+		printf("Cannot create socket.\n");
+		return 0;
+	}
+	else
+		printf("Socket creation successful (fd = %d)\n",server_socket);
+
+	// declare server address struct
+	sockaddr_in server_addr;
+
+	// initialize server address struct
+	bzero(&server_addr, sizeof(server_addr));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(port);
+
+	// print the port number (convert to readable format)
+	printf("Port: %d:\n", ntohs(server_addr.sin_port));
+
+	// bind address to socket
+	printf("Binding server IP to socket...\n");
+	if(bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0){
+		printf("Bind failed.\n");
+		return 0;
+	}
+	else
+		printf("Bind successful!\n");
+
+
+
 	//wait for input
-	while(strcmp(cmdbuf,"quit") != 0){
+	while(strcmp(cmdbuf,"exit") != 0){
+		
+		// clear command buffer
+	    bzero(cmdbuf, sizeof(char[50]));
+
 		printf("\nEnter command:");
 	    scanf("%s", cmdbuf); //accept a command from user
 
@@ -49,6 +95,12 @@ int main(int argc, char* argv[]){
 	    }
 
 	    else if(strcmp(cmdbuf,"myip") == 0){ //display user IP address
+	    	int temp_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	    	sockaddr_in tempaddr;
+	    	bzero(&tempaddr, sizeof(struct sockaddr_in));
+
+
+
 	    	printf("Your IP Address: %s\n",user_ip);
 	    }
 
